af31af8d2823eb3410d7bfe0e9b6fa2c86a2c8ee
Quinn M <captainq25@gmail.com>
2015-05-29 10:04:00 -0700

    site

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function show(shown, hidden, hidden2) {
+  document.getElementById(shown).style.display='block';
+  //document.getElementById(shown2).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  document.getElementById(hidden2).style.display='none';
+  //document.getElementById(hidden3).style.display='none';
+  return false;
+}
